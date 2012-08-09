a9cb61b0ca9d6cebf846a538bb2acf0a2a9da525
juliobarreraa <juliobarreraa@gmail.com>
2012-08-09 17:21:31 -0500

    Enabled checkbox added in form categories

--- protected/modules/admin/views/categories/_form.php
+++ protected/modules/admin/views/categories/_form.php
@@ -26,6 +26,13 @@
 		<?php echo $form->textField($model,'description',array('size'=>60,'maxlength'=>140)); ?>
 		<?php echo $form->error($model,'description'); ?>
 	</div>
+        
+	<div class="row">
+		<?php echo $form->labelEx($model,'enabled'); ?>
+		<?php echo $form->checkbox($model,'enabled'); ?>
+		<?php echo $form->error($model,'enabled'); ?>
+	</div>
+        
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
